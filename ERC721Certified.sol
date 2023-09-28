cb4217591a9c0c73388dc2eb10e74916cb1e6be4
melwong <melvinwonghc@gmail.com>
2023-09-28 11:30:23 +0900

    Create ERC721Certified.sol

---
+++ ERC721Certified.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.9.0/token/ERC721/ERC721.sol";
+
+contract ERC721Certified is ERC721 {
+
+    string public certificate;
+
+    constructor(string memory contractName, string memory contractSymbol, string memory cert)
+        ERC721(contractName, contractSymbol)
+    {
+        certificate = cert;
+    }
+}
