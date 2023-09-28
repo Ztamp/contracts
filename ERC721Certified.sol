0d22410fb2d7a90f9d6daf2cf05ddd1b779c2ba5
melwong <melvinwonghc@gmail.com>
2023-09-28 11:32:38 +0900

    Update ERC721Certified.sol

--- ERC721Certified.sol
+++ ERC721Certified.sol
@@ -5,11 +5,11 @@ import "@openzeppelin/contracts@4.9.0/token/ERC721/ERC721.sol";
 
 contract ERC721Certified is ERC721 {
 
-    string public certificate;
+    string public certificate;	// String public variable for certificate
 
     constructor(string memory contractName, string memory contractSymbol, string memory cert)
         ERC721(contractName, contractSymbol)
     {
-        certificate = cert;
+        certificate = cert;	// Assign certificate variable with your certificate ASCII code
     }
 }
