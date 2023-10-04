17667755d362326052e23a0f67fb5a8c9d9383e4
melwong <melvinwonghc@gmail.com>
2023-10-04 09:08:57 +0900

    Update ERC721Certified.sol

--- ERC721Certified.sol
+++ ERC721Certified.sol
@@ -2,14 +2,19 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts@4.9.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
 
-contract ERC721Certified is ERC721 {
+contract ERC721Certified is ERC721, Ownable {
 
-    string public certificate;	// String public variable for certificate
+    string public certificate;
 
     constructor(string memory contractName, string memory contractSymbol, string memory cert)
         ERC721(contractName, contractSymbol)
     {
-        certificate = cert;	// Assign certificate variable with your certificate ASCII code
+        certificate = cert;
+    }
+
+    function updateCert(string memory cert) public onlyOwner {
+        certificate = cert;
     }
 }
